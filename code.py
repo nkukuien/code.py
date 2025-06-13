2d411ff3536a3f5d8a450d48821bba167482ede1
nkukuien <129174393+nkukuien@users.noreply.github.com>
2025-06-12 21:51:54 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.title("Even or Odd Number Checker")
+
+num = st.number_input("Enter a number:", step=1)
+
+if st.button("Check"):
+    if num % 2 == 0:
+        st.success("The number is Even.")
+    else:
+        st.warning("The number is Odd.")
